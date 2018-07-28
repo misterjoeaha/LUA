9ed2d053215d1bb51582920319ac86f044949b04
misterjoeaha <misteranyjoe@hotmail.com>
2018-07-28 13:51:47 -0300

    Update funcoes.lua
    new ways with functions in LUA

--- funcoes.lua
+++ funcoes.lua
@@ -1,3 +1,5 @@
+
+
 function strlen(x)
     local k = #x
     if (k>15) then
@@ -18,3 +20,59 @@ function fat(x)
   end
 end
 x = 4
+---------------------------------------------------other_examples----------------------------------------------------
+v2 = {22,310,55,61,20,15,7,130,50,45,65,132,20} 
+for k=1 , #v2 do   -- tamanho da tabela
+  print(k ,v2[k])
+end
+do --Chunk
+	local maior = v2[1]
+	for i=2, #v2 do
+		if v2[i] > maior then
+			maior = v2[i]
+		end
+	end
+  print(maior) -- printa o maior valor da tabela
+end -- fim Chunk
+
+--- podemos escrever  tambem
+function maior(v) --Chunk
+	local maior = v2[1]
+	for i=2, #v2 do
+		if v2[i] > maior then
+			maior = v2[i]
+		end
+	end
+  return maior -- retorna o maior valor da tabela
+end -- fim Chunk
+
+---------------------------------------other_examples-----------------------------------------------------
+function soma(x, y)
+	return x + y
+end
+
+function prod(x, y)
+	return x * y
+end
+
+function exec(f ,x  , y)
+	return f(x,y)
+end
+
+funcs = (soma, 
+prod, 
+math.pow,
+function (a,b) return a - b end)
+
+a = 3
+b = 2
+
+res = {}
+for i = 1 , #funcs do
+	res[i] = exec(funcs[i], a , b)
+
+end 
+
+--for i = 1, #res do
+--	print(i,res[i])
+--end
